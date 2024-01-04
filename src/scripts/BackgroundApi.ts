ea9dcf1d2c4f6f1393f73d0bb91bf0d451655e6e
leejss <goldemshine@gmail.com>
2024-01-05 00:42:33 +0900

    init

---
+++ src/scripts/BackgroundApi.ts
@@ -0,0 +1,59 @@
+import { Observable, filter, fromEventPattern, lastValueFrom, take } from "rxjs";
+import Port from "./Port";
+import { v4 as uuid } from "uuid";
+export type BackgroundResponse<T = never> = {
+  id: string;
+  error: null | { code: number; msg: string };
+  data: null | T;
+};
+
+export default class BackgroundApi {
+  private port!: Port;
+  private portObservable!: Observable<BackgroundResponse>;
+
+  constructor() {
+    this.connect();
+  }
+
+  private connect() {
+    this.port = new Port(
+      { name: "BG" },
+      {
+        onConnect: (port) => {
+          this.initPortObservable(port);
+        },
+      },
+    );
+  }
+
+  private initPortObservable(port: chrome.runtime.Port) {
+    this.portObservable = fromEventPattern(
+      (handler) => port.onMessage.addListener(handler),
+      (handler) => port.onMessage.removeListener(handler),
+      (data) => data,
+    );
+  }
+
+  async call<Req, Res>(funcName: string, payload: Req): Promise<Res> {
+    const params = {
+      id: uuid(),
+      funcName,
+      payload,
+    };
+
+    this.port.postMessage(params);
+
+    const result = await lastValueFrom(
+      this.portObservable.pipe(
+        filter((data) => data.id === params.id),
+        take(1),
+      ),
+    );
+
+    if (result.error) {
+      throw new Error(result.error.msg);
+    }
+
+    return result.data as Res;
+  }
+}
---
+++ src/scripts/Port.ts
@@ -0,0 +1,40 @@
+export default class Port {
+  private chromePort!: chrome.runtime.Port;
+  private connected!: boolean;
+  private name!: string;
+  private callback!: (port: chrome.runtime.Port) => void | Promise<void>;
+
+  constructor(
+    connectInfo: chrome.runtime.ConnectInfo,
+    opts?: {
+      onConnect: (port: chrome.runtime.Port) => void;
+    },
+  ) {
+    if (!connectInfo.name) {
+      throw new Error("Port name is required");
+    }
+
+    this.name = connectInfo.name;
+    this.callback = opts?.onConnect ?? (() => {});
+    this.chromePort = this.createPort();
+  }
+
+  private createPort(): chrome.runtime.Port {
+    const newPort = chrome.runtime.connect({
+      name: this.name,
+    });
+    this.connected = true;
+    newPort.onDisconnect.addListener(() => {
+      this.connected = false;
+    });
+    this.callback(newPort);
+    return newPort;
+  }
+
+  postMessage<T>(msg: T) {
+    if (!this.connected) {
+      this.chromePort = this.createPort();
+    }
+    this.chromePort.postMessage(msg);
+  }
+}
